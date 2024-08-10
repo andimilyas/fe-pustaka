e27a787489928dbb2ca278fcad318c69414819ad
Andi <andimilyas123@gmail.com>
2024-08-10 23:39:29 +0700

    set vite

--- perpustakaan-app/vite.config.js
+++ perpustakaan-app/vite.config.js
@@ -1,16 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from 'vite' 
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+  return defineConfig({
+    plugins: [vue()],
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true, 
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  } 
-})
+  })
+}
